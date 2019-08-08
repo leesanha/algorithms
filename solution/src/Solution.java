1590e98eb6f72665bb09d75ac74105ee5acb0ebc
sana <bluegisa@daum.net>
2019-08-08 17:59:15 +0900

    add permutation

--- solution/src/Solution.java
+++ solution/src/Solution.java
@@ -1,61 +1,65 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Solution {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static long ans;
+	static Queue<String> q;
+	static boolean check[];
+	static int cnt;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		int tc = Integer.parseInt(br.readLine());
 		for (int t = 1; t <= tc; t++) {
-			char[] chs = br.readLine().toCharArray();
-			char[] frog = { 'c', 'r', 'o', 'a' };
-			Stack<Character> q1 = new Stack<Character>();
-			Stack<Character> q2 = new Stack<Character>();
+			String[] line = br.readLine().split(" ");
+			char[] num = line[0].toCharArray();
+			cnt = Integer.parseInt(line[1]);
+			ans = 0;
+			q = new LinkedList<String>();
+			check = new boolean[1000000];
 
-			int ans = 0;
-			int cnt = 0;
-
-			for (char ch : chs) {
-				if (q1.isEmpty() && ch != 'c') {
-					ans = -1;
-					break;
-				}
-				else if (ch == 'c') {
-					q1.push(ch);
-					cnt++;
-				} else if (ch == 'k') {
-					int idx = 3;
-					while (true) {
-						if (q1.isEmpty()) {
-							if (!q2.isEmpty()) {
-								ans = -1;
-							}
-							break;
-						}
-						if (q1.peek() != frog[idx]) {
-							q2.push(q1.pop());
-						} else {
-							q1.pop();
-							idx--;
-						}
-						if (idx == -1) {
-							while (!q2.empty()) {
-								q1.push(q2.pop());
-							}
-							cnt--;
-							break;
-						}
-					}
-				} else {
-					q1.push(ch);
-				}
-				if (ans != -1 && ans < cnt)
-					ans = cnt;
-			}
-			System.out.format("#%d %d\n", t, ans = (q1.empty() && q2.empty()) ? ans : -1);
+			dfs(num, 0, 0);
+			System.out.format("#%d %d\n", t, ans);
 		}
+	}
+
+	static void dfs(char[] num, int x, int idx) {
+		int temp = Integer.parseInt(String.valueOf(num));
+		System.out.println(new String(num) + " " + x);
+		if (cnt == idx) {
+//			System.out.println(new String(num) + " " + x);
+			if (temp > ans) {
+				ans = temp;
+			}
+			return;
+		}
+
+		for (int i = 0; i < num.length; i++) {
+			if (i == x)
+				continue;
+			if (x >= num.length) {
+				x = 0;
+			}
+			swap(num, x, i);
+			/*if (!check[temp]) {
+				dfs(num, x + 1, idx + 1);
+				check[temp] = true;
+			} else {
+				dfs(num, x + 1, idx);
+			}*/
+			dfs(num, x + 1, idx + 1);
+			swap(num, x, i);
+		}
+	}
+
+	static void swap(char[] num, int i, int depth) {
+		char temp = num[i];
+		num[i] = num[depth];
+		num[depth] = temp;
 	}
 
 }
