7de6b608b64d6f5a5165d37dd3fe01fbe45b5aa8
leesanha <bluegisa@daum.net>
2019-08-11 16:07:09 +0900

    for pull

--- solution/src/Solution.java
+++ solution/src/Solution.java
@@ -1,65 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
-import java.util.Queue;
+import java.util.PriorityQueue;
 
 public class Solution {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static long ans;
-	static Queue<String> q;
-	static boolean check[];
-	static int cnt;
+	static int camera, receiver, ans;
+	static LinkedList<Integer> list;
+	static PriorityQueue<Integer> q;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		int tc = Integer.parseInt(br.readLine());
-		for (int t = 1; t <= tc; t++) {
-			String[] line = br.readLine().split(" ");
-			char[] num = line[0].toCharArray();
-			cnt = Integer.parseInt(line[1]);
-			ans = 0;
-			q = new LinkedList<String>();
-			check = new boolean[1000000];
 
-			dfs(num, 0, 0);
+		for (int t = 1; t <= tc; t++) {
+			camera = Integer.parseInt(br.readLine());
+			receiver = Integer.parseInt(br.readLine());
+
+			String[] input = br.readLine().split(" ");
+
+			list = new LinkedList<Integer>();
+			for (int i = 0; i < camera; i++) {
+				int num = Integer.parseInt(input[i]);
+				if (!list.contains(num)) {
+					list.add(num);
+				}
+			}
+			Collections.sort(list);
+
+			q = new PriorityQueue<Integer>();
+			for (int i = 0; i < list.size() - 1; i++) {
+				q.add(Math.abs(list.get(i) - list.get(i + 1)));
+			}
+
+			/*
+			 * for(int i : list) { System.out.print(i + " "); } System.out.println();
+			 * for(int i : res) { System.out.print(i + " "); } System.out.println();
+			 */
+			ans = 0;
+			for (int i = 0; i <= receiver - 1; i++) {
+				q.poll();
+			}
+			while (!q.isEmpty()) {
+				ans += q.poll();
+			}
+
 			System.out.format("#%d %d\n", t, ans);
 		}
-	}
-
-	static void dfs(char[] num, int x, int idx) {
-		int temp = Integer.parseInt(String.valueOf(num));
-		System.out.println(new String(num) + " " + x);
-		if (cnt == idx) {
-//			System.out.println(new String(num) + " " + x);
-			if (temp > ans) {
-				ans = temp;
-			}
-			return;
-		}
-
-		for (int i = 0; i < num.length; i++) {
-			if (i == x)
-				continue;
-			if (x >= num.length) {
-				x = 0;
-			}
-			swap(num, x, i);
-			/*if (!check[temp]) {
-				dfs(num, x + 1, idx + 1);
-				check[temp] = true;
-			} else {
-				dfs(num, x + 1, idx);
-			}*/
-			dfs(num, x + 1, idx + 1);
-			swap(num, x, i);
-		}
-	}
-
-	static void swap(char[] num, int i, int depth) {
-		char temp = num[i];
-		num[i] = num[depth];
-		num[depth] = temp;
 	}
 
 }
